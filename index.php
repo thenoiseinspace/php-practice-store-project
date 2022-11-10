9d3f068174de60436ce24b868118c1e0c96dd0ac
Sarah Gilbreath <thenoiseinspace@gmail.com>
2022-11-10 14:07:58 -0500

    Trying to upload from htdocs folder

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP code</title>
+</head>
+<body>
+    <h1>Hello world --this is the one in the htdocs folder</h1>
+    
+</body>
+</html>
