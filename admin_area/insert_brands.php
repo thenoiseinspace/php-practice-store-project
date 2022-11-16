263a074acba0131cabdcada496ff9853045d9c66
Sarah Gilbreath <thenoiseinspace@gmail.com>
2022-11-16 15:43:35 -0500

    Insert brands into database and check for existing brands

--- admin_area/insert_brands.php
+++ admin_area/insert_brands.php
@@ -1,13 +1,37 @@
+<?php 
+include('../includes/connect.php');
+if(isset($_POST['insert_brand'])){
+    $brand_title=$_POST['brand_title']; 
+   
+    //select data from database
+    // note--those are backticks around brands, not single quotes
+    //this first one checks to make sure the brand that the user is trying to add isnt already in the database
+    $select_query="Select * from `brands` where brand_title='$brand_title'"; 
+    $result_select=mysqli_query($con,$select_query); 
+    $number=mysqli_num_rows($result_select);
+      if ($number>0){
+        echo "<script>alert('Brand already exists in database')</script>"; 
+      } else{
+
+    $insert_query="insert into `brands` (brand_title) values ('$brand_title')";
+    $result=mysqli_query($con,$insert_query); 
+    if($result){
+      echo "<script>alert('Brand has been inserted successfully')</script>"; 
+    }
+}}
+
+?>
+
 <form action="" method="post" class="mb-2">
 <div class="input-group w-90 mb-2">
   <span class="input-group-text bg-info" id="basic-addon1"><i class="fa-solid fa-receipt"></i>
   </span>
-  <input type="text" class="form-control" name="cat_title" placeholder="Insert brands" aria-label="brands" aria-describedby="basic-addon1">
+  <input type="text" class="form-control" name="brand_title" placeholder="Insert brands" aria-label="brands" aria-describedby="basic-addon1">
 </div>
 <div class="input-group w-10 mb-2 m-auto">
 
-  <!-- <input type="submit" class="form-control bg-info" name="insert_cat" value="Insert Brands" > -->
+  <input type="submit" class="bg-info border-0 p-2 my-3" name="insert_brand" value="Insert Brands" >
 
-  <button class="bg-info p-3 my-3 border-0">Insert Brands</button>
+  <!-- <button class="bg-info p-3 my-3 border-0">Insert Brands</button> -->
 </div>
 </form>
